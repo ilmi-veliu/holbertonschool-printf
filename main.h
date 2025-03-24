69db8d36ae49493dc1e5f7a982790c93e5718478
ilmi-veliu <iveliu011@gmail.com>
2025-03-24 14:36:29 +0100

    ajouts des premieres fonctions

--- main.h
+++ main.h
@@ -1,5 +1,19 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+void print_char (va_list args, char *format);
+void print_string(va_list args, char *format);
+void print_mod(va_list args, char *format);
+int _putchar(char c);
+
+typedef struct panda
+{
+	int (*fptr)(int, int);		
+} op_t;
+
+
 
 #endif
