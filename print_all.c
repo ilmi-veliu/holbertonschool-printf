62d3a6f70a8a09b84c3dc72f6e3abc9576ecbbdf
tmlgde <tom.lagarde33@outlook.fr>
2025-03-26 08:23:31 +0000

    ajout de la fonction print_string

--- print_all.c
+++ print_all.c
@@ -1,51 +1,18 @@
 #include "main.h"
 
-int i = 0;
-
-int print_char (va_list args, char *format)
-
+int print_string(va_list, args)
 {
-	if (!format)
-	return;
+	char *str = va_args(args, char*);
 
-	while (format[i])
+	int i = 0, count = 0;
+
+	if (!str)
+	str = NULL
+
+	while str[i]
 	{
-		_putchar(i);
+		_putchar(str[i])
+		count++;
 		i++;
-	}	
-}
-int print_string(va_list args, char *format)
-{
-	if (!format)
-	return;
-
-	while (format[i])
-	{
-		_putchar(i);
-		i++;
-	}	
-}
-
-int print_mod(va_list args, char *format)
-{
-	if (!format)
-	return;
-
-	while (format[i])
-	{
-		_putchar(i);
-		i++;
-	}	
-}
-
-int print_entier(va_list args, char *format)
-{
-	if (!format)
-	return;
-
-	while (format[i])
-	{
-		_putchar(i);
-		i++;
-	}	
-}
+	}
+	return count;
