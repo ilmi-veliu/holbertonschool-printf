0700cd6cc3842d3dc9fa3a43b1aeeebc369458d7
tmlgde <tom.lagarde33@outlook.fr>
2025-03-27 09:05:21 +0000

    ajout de prit_decimal, print_entier

--- print_all.c
+++ print_all.c
@@ -33,3 +33,46 @@ int print_char (va_list args)
 	(void)args;
 	return(_putchar('%'));
 }
+
+int print_decimal (va_list args)
+{
+	int n = va_arg(args, int);
+	unsigned int num;
+	int count = 0;
+	int digits[10];
+	int i = 0;
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num == 0)
+	{
+		count += _putchar('0');
+		return (count);
+	}
+
+	while (num > 0)
+	{
+		digits[i++] = num % 10;
+		num /= 10;
+	}
+
+	while (i > 0)
+	{
+		count += _putchar('0' + digits[--i]);
+	}
+
+	return (count);
+}
+
+int print_entier(va_list args)
+{
+	return (print_decimal);
+}
