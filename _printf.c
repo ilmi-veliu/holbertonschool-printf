7d9518bdab9ffe009dac8c7f70836aa5b3b6ad0d
ilmi-veliu <iveliu011@gmail.com>
2025-03-27 18:21:42 +0100

    _printf.c 3 e version

--- _printf.c
+++ _printf.c
@@ -6,39 +6,43 @@
  *
  * Return: Le nombre total de caractères imprimés (hors '\0').
  */
-int _printf(const char *format, ...)
+int	_printf(const char *format, ...)
 {
-	    va_list args;
-    unsigned int i = 0, count = 0;
-    int (*f)(va_list);
+	va_list args;
+	int count = 0, i = 0;
+	int (*f)(va_list);
 
-    if (format == NULL)
-	return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i])
-    {
-	if (format[i] == '%')
+	while (format[i] != '\0')
 	{
-	    f = get_op_func(&format[i + 1]);
-	    if (f != NULL)
-	    {
-		count += f(args);
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				return (-1);
+
+			f = get_op_func(&format[i]);
+			if (f != NULL)
+			{
+				count += f(args);
+			}
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
 		i++;
-	    }
-	    else
-	    {
-		count += _putchar(format[i]);
-	    }
 	}
-	else
-	{
-	    count += _putchar(format[i]);
-	}
-	i++;
-    }
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
