5bfcff32e0f0e7a06f789129c5be3238f67addbb
tmlgde <tom.lagarde33@outlook.fr>
2025-03-28 11:49:19 +0000

    resolution probleme github

--- main.h
+++ main.h
@@ -7,27 +7,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int _printf(const char *format, ...);
+int _printf(const char format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_decimal(va_list args);
 int _putchar(char c);
-int verif_spe(const char *format, unsigned int *i, va_list args);
+int verif_spe(const charformat, unsigned int i, va_list args);
 int print_entier(va_list args);
-int _print_deux(const char *format, int *i, va_list args);
+int _print_deux(const charformat, int *i, va_list args);
+
 /**
- * struct op - Structure associant un spécificateur à une fonction.
- * @spe: Caractère représentant le spécificateur (ex: 'c', 's', 'd').
- * @f: Pointeur vers la fonction correspondante prenant va_list en paramètre.
- *
- * Description: Cette structure est utilisée pour associer un spécificateur
- * de format à une fonction qui effectue l'affichage approprié.
- */
+ 
+struct panda - Structure associant un spécificateur |  une fonction.
+@specifier: Chaîne représentant le spécificateur (ex: "c", "s", "d").
+@f: Pointeur vers la fonction correspondante prenant va_list en paramètre*
+Description: Cette structure est utilisée pour associer un spécificateur
+de format à une fonction qui effectue l'affichage approprié.*/
 typedef struct op
 {
-	char spe;
-	int (*f)(va_list);
+    char spe;
+    int (f)(va_list);
 } op_t;
+
+/ Déclaration du tableau verif_carac[] /
 extern op_t verif_carac[];
-#endif
+
+#endif / MAIN_H */
--- verif_carac.c
+++ verif_carac.c
@@ -1,58 +1,39 @@
 #include "main.h"
-/**
-<<<<<<< HEAD
- * verif_carac - Tableau des spécifieurs de format et d fonctions associées
- *
- * Ce tableau associe chaque spécifieur (`%c`, `%s`, `%d`, etc.) la fonction
- * qui permet de l'afficher correctement.
- */
-=======
- * verif_spe - Vérifie et applique un spécifieur de format
- * @format: Chaîne de format
- * @i: Index actuel dans la chaîne
- * @args: Liste des arguments variadiques
- *
- * Return: Le nombre de caractères affichés.
- */
-int verif_spe(const char *format, unsigned int *i, va_list args)
-{
->>>>>>> 8c1a632ddf43bae48d065724256740512f2b93ab
+
+/
+ 
+verif_carac - Tableau des spécifieurs et de leurs fonctions associées*/
 op_t verif_carac[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_decimal},
-		{'i', print_decimal},
-		{'\0', NULL}
-	};
+    {'c', print_char},
+    {'s', print_string},
+    {'%', print_percent},
+    {'d', print_decimal},
+    {'i', print_decimal},
+    {'\0', NULL}
+};
 
-<<<<<<< HEAD
-
-/**
- * verif_spe - Vérifie et applique un spécifieur de format
- * @format: Chaîne de format
- * @i: Index actuel dans la chaîne
- * @args: Liste des arguments variadiques
- *
- * Return:  le nombre de caractères affichés.
- */
-int verif_spe(const char *format, unsigned int *i, va_list args)
+/
+ 
+verif_spe - Vérifie et applique un spécifieur de format
+@format: Chaîne de format
+@i: Index actuel dans la chaîne
+@args: Liste des arguments variadiques*
+Return: Le nombre de caractères affichés.*/
+int verif_spe(const char format, unsigned inti, va_list args)
 {
-=======
->>>>>>> 8c1a632ddf43bae48d065724256740512f2b93ab
-	int j, count = 0;
+    int j, count = 0;
 
-	for (j = 0; verif_carac[j].spe != '\0'; j++)
-	{
-		if (verif_carac[j].spe == format[*i])
-		{
-			count += verif_carac[j].f(args);
-			return (count);
-		}
-	}
+    for (j = 0; verif_carac[j].spe != '\0'; j++)
+    {
+        if (verif_carac[j].spe == format[i])
+        {
+            count += verif_carac[j].f(args);
+            return (count);
+        }
+    }
 
-	count += _putchar('%');
-	count += _putchar(format[*i]);
+    count += _putchar('%');
+    count += _putchar(format[i]);
 
-	return (count);
+    return (count);
 }
