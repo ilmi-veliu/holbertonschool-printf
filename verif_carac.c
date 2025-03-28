e45c762554f736a20942c76807143ee0c7dc3c3b
ilmi-veliu <iveliu011@gmail.com>
2025-03-28 12:03:57 +0100

    erreur betty l 31 dans verif_carcac.c

--- verif_carac.c
+++ verif_carac.c
@@ -1,5 +1,14 @@
 #include "main.h"
-
+/**
+ * verif_spe - Gère un spécificateur de format.
+ * @format: Chaîne contenant les spécificateurs.
+ * @i: Pointeur vers l'index actuel dans la chaîne.
+ * @args: Liste des arguments variadiques.
+ *
+ * Return: Nombre de caractères imprimés.
+ */
+int verif_spe(const char *format, unsigned int *i, va_list args)
+{
 op_t verif_carac[] = {
 		{'c', print_char},
 		{'s', print_string},
@@ -9,10 +18,6 @@ op_t verif_carac[] = {
 		{'\0', NULL}
 	};
 
-
-
-int verif_spe(const char *format, unsigned int *i, va_list args)
-{
 	int j, count = 0;
 
 	for (j = 0; verif_carac[j].spe != '\0'; j++)
@@ -23,7 +28,6 @@ int verif_spe(const char *format, unsigned int *i, va_list args)
 			return (count);
 		}
 	}
-
 	count += _putchar('%');
 	count += _putchar(format[*i]);
 
