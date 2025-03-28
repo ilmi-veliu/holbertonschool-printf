1a6400c7897e9eabb694d824e517033e72c89cde
ilmi-veliu <iveliu011@gmail.com>
2025-03-28 18:28:49 +0100

    betty parfait

--- verif_carac.c
+++ verif_carac.c
@@ -1,40 +1,36 @@
 #include "main.h"
 
 /**
- * verif_carac - Tableau des spécifieurs et de leurs fonctions associées
- */
-op_t verif_carac[] = {
-    {'c', print_char},
-    {'s', print_string},
-    {'%', print_percent},
-    {'d', print_decimal},
-    {'i', print_decimal},
-    {'\0', NULL}
-};
-
-/**
  * verif_spe - Vérifie et applique un spécifieur de format
  * @format: Chaîne de format
  * @i: Index actuel dans la chaîne
  * @args: Liste des arguments variadiques
  *
- * Return: Le nombre de caractères affichés.
+ * Return: Le nombre de caractères affichés
  */
 int verif_spe(const char *format, unsigned int i, va_list args)
 {
-    int j, count = 0;
+	int j, count = 0;
+	op_t verif_carac[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_decimal},
+		{'i', print_decimal},
+		{'\0', NULL}
+	};
 
-    for (j = 0; verif_carac[j].spe != '\0'; j++)
-    {
-        if (verif_carac[j].spe == format[i])
-        {
-            count += verif_carac[j].f(args);
-            return (count);
-        }
-    }
+	for (j = 0; verif_carac[j].spe != '\0'; j++)
+	{
+		if (verif_carac[j].spe == format[i])
+		{
+			count += verif_carac[j].f(args);
+			return (count);
+		}
+	}
 
-    count += _putchar('%');
-    count += _putchar(format[i]);
+	count += _putchar('%');
+	count += _putchar(format[i]);
 
-    return (count);
+	return (count);
 }
