2aadd9d88def0a1368e522076507793d988aa247
tmlgde <tom.lagarde33@outlook.fr>
2025-03-31 12:16:51 +0000

    betty

--- verif_carac.c
+++ verif_carac.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
-/**
- * verif_carac - Tableau des spécifieurs et de leurs fonctions associées
- */
-op_t verif_carac[] = {
-    {'c', print_char},
-    {'s', print_string},
-    {'%', print_percent},
-    {'d', print_decimal},
-    {'i', print_decimal},
-    {'\0', NULL}
+static op_t verif_carac[] = {
+	{'c', print_char},
+	{'s', print_string},
+	{'%', print_percent},
+	{'d', print_decimal},
+	{'i', print_decimal},
+	{'\0', NULL}
 };
 
 /**
@@ -22,19 +19,19 @@ op_t verif_carac[] = {
  */
 int verif_spe(const char *format, unsigned int i, va_list args)
 {
-    int j, count = 0;
+	int j, count = 0;
 
-    for (j = 0; verif_carac[j].spe != '\0'; j++)
-    {
-        if (verif_carac[j].spe == format[i])
-        {
-            count += verif_carac[j].f(args);
-            return (count);
-        }
-    }
+	for (j = 0; verif_carac[j].spe != '\0'; j++)
+	{
+		if (verif_carac[j].spe == format[i])
+		{
+			count += verif_carac[j].f(args);
+			return (count);
+		}
+	}
 
-    count += _putchar('%');
-    count += _putchar(format[i]);
+	count += _putchar('%');
+	count += _putchar(format[i]);
 
-    return (count);
+	return (count);
 }
